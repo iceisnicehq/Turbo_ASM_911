0f84462695d42c3cbac70c86ef4f163ca58b79e7
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-09-10 22:25:27 +0300

    Update lab.asm

--- lab.asm
+++ lab.asm
@@ -2,39 +2,22 @@
     ;--------------------------------------
     ; d = (a + 12*b*c+6) / (65*c+7*a^2)
     ;--------------------------------------
+    ; overflow is when the resulting value is bigger than 16bits
 
-calc:
-    mov ax, 7*a ;ax <- 7*a
-    imul ax, a  ;ax <- 7*a^2
-    ; check of
-    xchg ax, bx ;ax <- 0, bx <- 7*a^2
-    mov ax, c
-    imul ax, 65
-    add ax, bx
-    ; check of
-    xchg ax, cx ;ax <- 0, cx <- 65*c+7*a^2
-    mov ax, 12*b ; ax <- 12*b
-    imul ax, c ; ax <- 12*b*c
-    add ax, a ; ax <- a+12*b*c
-    adc ax, 6 ; ax <- a+12*b*c+6
-    idiv cx ; ax/cx
-    mov cx, d ; ???????????????????
-    
 .model small
 .stack 100h
 
 .data
     ; ?? -128 ?? 127 <=> 80h to 7Fh
-    a_loop db 255
-    b_loop db 255
-    c_loop db 255
-    path db 'D:\NEWFILE.TXT', 0
-    a db 0
+    a db 127
     b db 127
-    c db 12
-    d db ?
+    c db 127
+    count_a db 10
+    count_b db 30 
+    count_c db 30
+    path db 'D:\NEWFILE.TXT', 0
+    buffer db "A = 0000, B = 0000, C = 0000", 0ah
     handle dw ?
-    buffer db 8 dup (?)
     ; buffer should be like "A = ----, B = ----, C = ----\n" which is 29 chars
     
 .code 
@@ -58,52 +41,70 @@ mkFile:
     int 21h
     mov [handle], ax
 
-if_undefined:
+    ;if_undefined:
 loop_a:
-    ; a goes from -128 to 127
+    mov [count_b], 127
+    ;mov b, 
 loop_b:
-    ; b goes from -128 to 127
-    ; b_loop dec by one
+    mov [count_c], 127
+    ;mov c,
 loop_c:
-    ; c goes from -128 to 127
-    ; c_loop dec by one
-    ; when c_loop = 0
-    ; jnz loop_c
-    ; mov c_loop, 255
-
-    ; jmp loop_b
-    ; when b_loop = 0
-
-    ; jnz loop_b
-    ; mov b_loop, 255
-    ; jmp loop_a
-
-; a_loop dec by one
-; when a_loop = 0
-; jnz loop_a
-
-;make wrFile function for usage for every possible 
-;wr file should check whether the value passed in a variable is negative
-    ;this should work by checking and then neg it to positive value
-    ; with either jump or with test of the MSB
+;    jmp wrBuffer
+;calc:
+;    mov ax, 7*a ;ax <- 7*a
+;    imul ax, a  ;ax <- 7*a^2
+;    ; check of
+;    xchg ax, bx ;ax <- 0, bx <- 7*a^2
+;    mov ax, c
+;    imul ax, 65
+;    add ax, bx
+;    ; check of
+;    xchg ax, cx ;ax <- 0, cx <- 65*c+7*a^2
+;    mov ax, 12*b ; ax <- 12*b
+;    imul ax, c ; ax <- 12*b*c
+;    add ax, a ; ax <- a+12*b*c
+;    adc ax, 6 ; ax <- a+12*b*c+6
+;    idiv cx ; ax/cx
+;    ;jno loop_c
+wrBuffer:
+    ;mov [buffer], 041h ;A
+    ;mov [buffer+1], 00h ;A 
+    ;mov [buffer+2], 3dh ;A =
+    mov al, a
+    aam
+    or al, 30h
+    mov [buffer+7], al  ;A = 000-, B = 0000, C = 0000\n
+    mov al, ah
+    aam
+    or ax, 3030h
+    mov [buffer+6], al ;A = 00--, B = 0000, C = 0000\n
+    mov [buffer+5], ah ;A = 0---, B = 0000, C = 0000\n
+    ;write b
+    mov al, b
+    aam
+    or al, 30h
+    mov [buffer+17], al  ;A = 0000, B = 0000, C = 0000\n
+    mov al, ah
+    aam
+    or ax, 3030h
+    mov [buffer+16], al ;A = 00-0, B = 0000, C = 0000\n
+    mov [buffer+15], ah ;A = 0--0, B = 0000, C = 0000\n
+    ;write c
+    mov al, c
+    aam
+    or al, 30h
+    mov [buffer+27], al  ;A = 0000, B = 0000, C = 0000\n
+    mov al, ah
+    ;aam
+    ;or ax, 3030h
+    ;mov [buffer+20], al ;A = 00-0, B = 0000, C = 0000\n
+    ;mov [buffer+19], ah ;A = 0--0, B = 0000, C = 0000\n
+    ;mov [buffer+7], 0ah;A = 0---, B = 0000, C = 0000\n
+    ;mov [buufer
+    
 wrFile:
-        mov [buffer], 041h
-        mov [buffer+1], 00h
-        mov [buffer+2], 3dh
-        mov al, a
-        aam
-        or ax, 0030h
-        mov [buffer+3], 00h
-        mov [buffer+6], al
-        mov al, ah
-        aam
-        or ax, 3030h
-        mov [buffer+5], al
-        mov [buffer+4], ah
-        mov [buffer+7], 0ah
-        mov [buufer
-        mov dx, offset buffer
-        mov cx, 8
+    mov dx, offset buffer
+    mov cx, 29
     mov ah, 40h
     mov bx, handle
     int 21h
@@ -111,6 +112,23 @@ wrFile:
     jz clFile
     inc [a]
     jmp wrFile
+    ; calc
+    ; write
+    dec c
+    dec [count_c]
+    jnz loop_c
+    dec b
+    dec [count_b]
+    jnz loop_b
+    dec a
+    dec [count_a]
+    jnz loop_a
+
+;make wrFile function for usage for every possible 
+;wr file should check whether the value passed in a variable is negative
+    ;this should work by checking and then neg it to positive value
+    ; with either jump or with test of the MSB
+
     
 clFile:
     mov ah, 3Eh
@@ -122,4 +140,5 @@ Exit:
     mov al, 0
     int 21h
     End Start
+    
     
