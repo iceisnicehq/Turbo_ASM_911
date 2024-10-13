806356293da691a35fccc25187711c8c79fe616c
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-10-13 14:39:11 +0300

    working

--- prod.asm
+++ prod.asm
@@ -4,139 +4,166 @@
 .186
 .stack    100h
 
-;KOHCTAHTbI
 MIN            EQU    -128
 MAX            EQU    127
-CYCLES         EQU    255 - MAX + MIN
-MIN&CYC        EQU    MIN * 100h + CYCLES
 
+JUMPS 
 .data
     path       db    'OUTASM.TXT', 0
-    buffer     db    "A =  000, B =  000, C =  000", 0dh, 0ah
+    buffer     db    "A = 0000, B = 0000, C = 0000", 0dh, 0ah
 .data?
-    count_a    db    ?
     a          db    ?
-    count_b    db    ?
-    b          db    ?
-    count_c    db    ?
     c          db    ?
-
-
+    b          db    ?
+    d          dw    ?
 .code
 Start:
     mov    ax,    @data                  
-    mov    ds,    ax   
-    mov    al,    byte ptr [a]           
-    or     al,    byte ptr [c]           
-    jnz    calc              
-    mov    ax,    MIN&CYC
-    mov    word ptr [count_a],   ax                      
-    mov    word ptr [count_b],   ax
-    mov    word ptr [count_c],   ax  
-    mov    di,    offset buffer
+    mov    ds,    ax  
+    mov    es,    ax
+    mov    si,    offset a  
+    lodsw       
+    or     al,    ah   
+    jnz    get_val      
 mkFile:
     mov    dx,    offset path            
     mov    ah,    03Ch                   
     xor    cx,    cx                     
-    int    21h                           
-    mov    si,    ax                     
+    int    21h                            
+init:
+    lea    di,    [buffer + 26]
+    std 
+    mov    ah,    MIN
+    mov    si,    ax
+    mov    al,    ah 
+    mov    bp,    ax
+    jmp    SHORT calc  
+get_val:
+    mov    ch,    al
+    lodsb
+    mov    di,    si
+    mov    si,    ax 
+    mov    cl,    al
+    mov    bp,    cx  
     ; EQUATION    d = a + 12*b*c +6 / 65*c + 7*a^2
 calc:
-    mov    al,    byte ptr [a]
-    mov    bl,    al  
-    imul   al
-    cmp    ax,    5929d
-    jae    wrBuffer   
-    xor    bp,    bp
-    mov    dx,    ax                     
-    sal    ax,    1
-    add    dx,    ax
-    sal    ax,    1
-    add    dx,    ax
-    jno    no_of
-    mov    bp,    20000d
-    sub    dx,    bp
-no_of:
-    mov    al,    byte ptr [c]           
-    mov    bh,    al   
-    cbw    
-    mov    cx,    ax                     
-    sal    cx,    6                      
+    mov    ax,    bp
+    cbw
+    mov    bx,    ax
+    sal    ax,    3
+    sub    ax,    bx
+    imul   bx
+    mov    bh,    ch
+    or     dx,    dx
+    jnz    wrBuffer
+    mov    dx,    ax
+    mov    ax,    si
+    mov    al,    ah
+    cbw
+    mov    cx,    ax
+    sal    ax,    6
     add    cx,    ax
-    add    cx,    bp
+    js     negative
     add    cx,    dx
-    jo     wrBuffer
-    or     si,    si
+    js     wrBuffer
+    jc     wrBuffer
+    jmp    SHORT check_loop
+negative:
+    neg    cx
+    sub    dx,    cx
+    jc     check_loop
+    js     wrBuffer
+check_loop:
+    test   si,    00FFh
     jnz    loop_iter
     jmp    numerator
-wrBuffer:   
-    mov    al,    byte ptr [a]
-    test   al,    080h                   
-    jns    posA
-    neg    al
-    mov    byte ptr [di + 4],     02dh      
-posA:
-    aam
-    or     al,    30h
-    mov    byte ptr [di + 7],    al      
-    mov    al,    ah
-    aam
-    or     ax,    3030h
-    xchg   ah,    al
-    mov    word ptr [di + 5],    ax        
-    mov    al,    byte ptr [b]
-    test   al,    080h                    
-    jns    posB
-    neg    al
-    mov    byte ptr [di + 14],    2dh     
-posB:
-    aam
-    or     al,    30h
-    mov    byte ptr [di + 17],    al     
-    mov    al,    ah
-    aam
-    or     ax,    3030h
-    xchg   ah,    al    
-    mov    word ptr [di + 15],    ax     
-    mov    al,    byte ptr [c]
+wrBuffer:
+    mov    bx,    di
+    mov    dx,    202dh
+    mov    cl,    dh
+    mov    ax,    si
+    mov    al,    ah   
     test   al,    080h                    
     jns    posC
     neg    al
-    mov    byte ptr [di + 24],    2dh        
+    mov    cl,    dl       
 posC:
     aam
-    or     al,    30h
-    mov    byte ptr [di + 27],    al     
+    mov    ch,    al
     mov    al,    ah
     aam
+    xchg   ch,    ah
     or     ax,    3030h
-    xchg   ah,    al
-    mov    word ptr [di + 25],    ax    
+    stosw   
+    mov    ax,    cx
+    or     ah,    30h
+    stosw
+    sub    di,    6
+    mov    ax,    bp
+    mov    al,    ah
+    mov    cl,    dh
+    test   al,    080h                    
+    jns    posB
+    neg    al
+    mov    cl,    dl    
+posB:
+    aam
+    mov    ch,    al
+    mov    al,    ah
+    aam
+    xchg   ch,    ah
+    or     ax,    3030h
+    stosw   
+    mov    ax,    cx
+    or     ah,    30h
+    stosw
+    sub    di,    6
+    mov    ax,    bp
+    mov    cl,    dh
+    test   al,    080h                   
+    jns    posA
+    neg    al
+    mov    cl,    dl     
+posA:
+    aam
+    mov    ch,    al
+    mov    al,    ah
+    aam
+    xchg   ch,    ah
+    or     ax,    3030h
+    stosw   
+    mov    ax,    cx
+    or     ah,    30h
+    stosw
+    sub    di,    2
 wrFile:
     mov    dx,    di
+    mov    di,    bx
     mov    cx,    30
     mov    ah,    40h
     mov    bx,    si
+    xor    bh,    bh
     int    21h
-    mov    byte ptr [di +  4],     020h 
-    mov    byte ptr [di + 14],     020h 
-    mov    byte ptr [di + 24],     020h 
+
 loop_iter:
-    add    word ptr [count_c],     0101h
-    cmp    byte ptr [count_c],     00h
-    jz     c_max
-    jmp    calc
-c_max:
-    mov    word ptr [count_c],    MIN&CYC
-    add    word ptr [count_b],    0101h
-    cmp    byte ptr [count_b],    00h
-    jz     b_max
-    jmp    calc
-b_max:
-    mov    word ptr [count_b],    MIN&CYC
-    add    word ptr [count_a],    0101h
-    cmp    byte ptr [count_a],    00h
-    jz     clFile
+    mov    ax,    si
+    add    si,    0100h
+    cmp    ah,    MAX
+    jl     not_max
+    mov    ax,    si
+    mov    ah,    MIN
+    mov    si,    ax
+    mov    ax,    bp
+    add    bp,    0100h
+    cmp    ah,    MAX
+    jl     not_max
+    mov    cl,    al
+    inc    al
+    mov    ah,    MIN
+    mov    bp,    ax
+    cmp    cl,    MAX
+    jnl     clFile
+not_max:
     jmp    calc
 clFile:
     mov    ah,    3Eh
@@ -144,23 +171,21 @@ clFile:
     int    21h
     jmp    SHORT Exit
 numerator:
-    mov    al,    bh 
+    mov    ax,    bp
+    mov    al,    ah 
     cbw
     mov    dx,    ax    
     sal    dx,    1
     add    dx,    ax
-    sal    dx,    1                      
-    sal    dx,    1
-    mov    al,    bl
+    sal    dx,    2
+    mov    ax,    si
+    mov    al,    ah
     cbw
-    mov    bx,    ax 
-    mov    al,    byte ptr [b]           
-    cbw                                  
     imul   dx
     add    bx,    6
     js     neg_bx
     add    ax,    bx
-    adc    dx,    0
+    adc    dx,    0s
     jmp    SHORT division
 neg_bx:
     neg    bx
@@ -168,6 +193,7 @@ neg_bx:
     sbb    dx,    0
 division:
     idiv   cx
+    stosw     
 Exit:
     mov    ah,    04Ch
     mov    al,    0
