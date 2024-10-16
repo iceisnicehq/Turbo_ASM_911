5d5e8c9d93ee13921807754bb8b1123923b9a3a7
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-10-16 12:05:47 +0300

    Create lab2.asm

---
+++ lab2.asm
@@ -0,0 +1,7 @@
+;limit 256 bytes
+; word is everything up to to a \s character
+; if the word that needs to be reversed doesnt exist a program should throw an error
+; the input is in cmd
+; enter should mark the end
+
+; can i use stack???
