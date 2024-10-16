f3fb3df8518e8f933fdf1bd3b93fb83d6ca4522b
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-10-16 15:16:32 +0300

    Update lab2.asm

--- lab2.asm
+++ lab2.asm
@@ -1,7 +1,33 @@
-;limit 256 bytes
+; limit 256 bytes
 ; word is everything up to to a \s character
 ; if the word that needs to be reversed doesnt exist a program should throw an error
 ; the input is in cmd
 ; enter should mark the end
 
 ; can i use stack???
+; proccessor model???
+
+; С использованием строковых команд собрать в выделенном буфере текст, 
+; включающий в себя каждое пятое слово исходного текста. 
+; При этом второе слово собранного вами текста должно быть инвертированным по порядку букв. 
+; Исходный текст вводится с клавиатуры. 
+; Вывод собранного текста – на экран и в файл.
+
+.model small
+.486
+.stack 100h
+
+cr EQU 13d
+lf EQU 10d
+
+.data
+    buffer ? dup (258)
+
+.code
+Start:
+
+Exit:
+    mov ah, 04ch
+    mov al, 0
+    int 21h
+    END Start
