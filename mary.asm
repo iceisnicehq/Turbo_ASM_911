e9df5a0c3ec0e1424cac7d1347d5da860bedd0e1
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-10-18 12:18:26 +0000

    hell

--- mary.asm
+++ mary.asm
@@ -2,20 +2,23 @@
 .186
 .stack 100h
 .data
-A   db  ?
 C   db  ?
+A   db  ?
 B   db  ?
 FileName    db  "overflow.txt", 0
 handle  dw  ?
 buffer  db  '0000, 0000, 0000', 0Dh, 0Ah
 .code
 start:
+; cx: ch  = b, cl = desc
+; bx: bh  = a, bl = c
+
     mov     ax, @data
     mov     ds, ax
-    mov     bh, A
-    mov     bl, C
+    mov     bx, word ptr A
     mov     cl, B
-    cmp     bx, 0000h
+    ;;;;;;;;;cmp     bx, 0000h
+    or      bx, bx
     jne     example
     mov     ah, 3ch
     xor     cx, cx
@@ -26,61 +29,110 @@ start:
     mov     bh, al ;A
     mov     bl, al ;C
     mov     cl, al ;B
-example: 
-    mov     al, bl
-    test    al, 080h
-    
-
-
-
-
+example:  ; EXAMPLE = 9*a^2 + 42*b - c / a^3 + 6*c
+    ; bh = a
+    ; bl = c
+    ; cl = b
 
     mov     al, bl  
     cbw           
-    sal     ax, 1   
-    mov     si, ax  
-    sal     ax, 1   
-    add     si, ax 
-    
+    mov     si, ax
+    sal     ax, 1
+    add     si, ax  ; 3c
+
     mov     al, bh 
     imul    al      
     mov     bp, ax      
     mov     al, bh  
     cbw             
     imul    bp 
-    mov     dx, ax 
-    jo      wof
-    add     si, dx  
-    jz      next    
-    mov     ax, bp
-    sal     ax, 1
-    sal     ax, 1
-    sal     ax, 1
-    add     bp, ax
-    mov     al, cl ;al=B  
-    cbw
-    mov     dx, ax
-    sal     ax, 1
-    sal     ax, 1
-    add     ax, dx
-    sal     dx, 1
-    sal     dx, 1
-    sal     dx, 1
-    sal     dx, 1
-    add     ax, dx
-    sal     ax, 1
-    mov     di, ax  
-    mov     al, bl  
-    cbw  
-    add     bp, di  
-    sub     bp, ax  
-    mov     ax, bp
-    cwd
-    idiv    si
+    js      nega3
+    or      dx, dx
+    jnz     wof
+LOCALS @@
+    mov     di, ax
+    sal     si, 1   ; 6c
+    jns     @@pos
+    neg     si
+    sub     di, si
+    jc      next
+    js      wof
+    jmp     check
+@@pos:
+    add     di, si
+    js      wof
+    jc      wof
+    ; posit ax
+    jmp     check
+
+nega3:
+    cmp     dx, 0ffffh
+    jnz     wof
+    ; negative ax
+    mov     di, ax
+    sal     si, 1   ; 6c 
+    jns     @@pos
+    neg     si
+    sub     di, si
+    jc      wof
+    jns     wof
+        jmp     check
+
+@@pos:
+    add     di, ax
+    jns    wof
+    jnc    wof
+check:
     cmp     [handle], 0
-    jnz     next
-    mov     ah, 4Ch
-    int     21h
+    jnz    next
+;    jmp    chislitel
+; chislitel:
+;     mov     al, bl  
+;     cbw           
+;     sal     ax, 1   
+;     mov     si, ax  
+;     sal     ax, 1   
+;     add     si, ax 
+    
+;     mov     al, bh 
+;     imul    al      
+;     mov     bp, ax      
+;     mov     al, bh  
+;     cbw             
+;     imul    bp 
+;     mov     dx, ax 
+;     jo      wof
+;     add     si, dx  
+;     jz      next    
+;     mov     ax, bp
+;     sal     ax, 1
+;     sal     ax, 1
+;     sal     ax, 1
+;     add     bp, ax
+;     mov     al, cl ;al=B  
+;     cbw
+;     mov     dx, ax
+;     sal     ax, 1
+;     sal     ax, 1
+;     add     ax, dx
+;     sal     dx, 1
+;     sal     dx, 1
+;     sal     dx, 1
+;     sal     dx, 1
+;     add     ax, dx
+;     sal     ax, 1
+;     mov     di, ax  
+;     mov     al, bl  
+;     cbw  
+;     add     bp, di  
+;     sub     bp, ax  
+;     mov     ax, bp
+;     cwd
+;     idiv    si
+;     cmp     [handle], 0
+;     jnz     next
+;     mov     ah, 4Ch
+;     int     21h
 next:
     inc     bl
     cmp     bl, 128d
