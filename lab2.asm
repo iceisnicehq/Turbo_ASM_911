0250a66f590affa1340d374c5b70242b731993c4
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-10-30 19:41:32 +0300

    Update lab2.asm

--- lab2.asm
+++ lab2.asm
@@ -84,7 +84,7 @@ read_char:
     cmp     al,   0dh           
     je      end_input
     cmp     al,   7fh
-    je      read_char
+    jg      read_char
     cmp     al,   0ah
     je      read_char
     cmp     al,   09h
