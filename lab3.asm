762e1a640c94ac8228e5e0d96de00f66b7f9dda4
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-11-28 22:40:09 +0300

    Update lab3.asm

--- lab3.asm
+++ lab3.asm
@@ -4,83 +4,128 @@
 .386 
 .stack 100h 
 .data 
-A db 7Fh 
-B db 7Fh 
-C db 7Fh 
-D dd ? 
+    db 32 dup(?)
+    a          db    ?
+    c          db    ?
+    b          db    ?
+    d          dw    ?
 .code 
-start: 
-    mov ax,@data 
-    mov ds,ax 
-    mov es,ax 
+start:
+    ; ASSUME  es:STACK 
+    mov     ax,    @data 
+    mov     ds,    ax 
+    ;mov es,ax 
+    push    offset A
+    mov     si,    sp
+    ; lea si,A 
+    ; push si 
+    ; lea si,B 
+    ; push si 
+    ; lea si,C 
+    ; push si 
+    ; lea si,D 
+    ; push si 
     
-    xor ax,ax 
-    lea si,A 
-    push si 
-    lea si,B 
-    push si 
-    lea si,C 
-    push si 
-    lea si,D 
-    push si 
+    call    calc 
+exit:
+    mov     ax,    4C00h 
+    int     21h 
     
-    call vizov 
-  
-znamenatelzero: 
-    mov ah,4ch 
-    int 21h 
+calc proc near 
+    ; EQUATION    d = a + 12*b*c +6 / 65*c + 7*a^2
+    lods    word ptr ss:[si]
+    mov     al,    bl
+    cbw
+    mov    bp,    ax
+    sal    ax,    3
+    sub    ax,    bp
+    imul   bp
+    or     dx,    dx
+    jnz    wrBuffer
+    mov    dx,    ax
+    mov    ax,    si
+    mov    al,    ah
+    cbw
+    mov    cx,    ax
+    sal    ax,    6
+    add    cx,    ax
+    js     negative
+    add    dx,    cx
+negative:
+    neg    cx
+    sub    dx,    cx
+numerator:
+    mov    cx,    dx
+    mov    al,    bh
+    cbw
+    mov    dx,    ax    
+    sal    dx,    1
+    add    dx,    ax
+    sal    dx,    2
+    mov    ax,    si
+    mov    al,    ah
+    cbw
+    imul   dx
+    add    bp,    6
+    js     neg_bx
+    add    ax,    bp
+    adc    dx,    0
+neg_bx:
+    neg    bx
+    sub    ax,    bx
+    sbb    dx,    0
+division:
+    idiv   cx
+    stosw     
+    ; mov bp,sp 
+    ; mov si,[bp+10] 
+    ; mov bl,[si] 
+    ; mov si,[bp+6] 
+    ; mov cl,[si] 
+    ; mov si,[bp+8] 
+    ; mov bh,[si] 
     
-    vizov proc near 
-    push bp 
-    mov bp,sp 
-    mov si,[bp+10] 
-    mov bl,[si] 
-    mov si,[bp+6] 
-    mov cl,[si] 
-    mov si,[bp+8] 
-    mov bh,[si] 
     
     
+    ; MOV AL,CL 
+    ; IMUL BL 
+    ; CWDE 
+    ; SAL EAX,3 
+    ; MOV EDI,EAX 
+    ; MOVSX AX,BH 
+    ; MOV SI,AX 
+    ; SAL SI,6 
+    ; ADD SI,AX 
+    ; SAL AX,3 
+    ; ADD SI,AX 
+    ; MOVSX ESI,SI 
+    ; SUB ESI,EDI 
+    ; MOVSX EAX,BL 
+    ; ADD EAX,ESI 
+    ; ; JZ znamenatelzero 
+    ; MOV ESI,EAX 
+    ; MOV AL,CL 
+    ; IMUL CL 
+    ; CWDE 
+    ; MOV EDI,EAX 
+    ; MOV AL,BH 
+    ; IMUL BH 
+    ; MOV CX,AX 
+    ; MOVSX AX,BH 
+    ; IMUL CX 
+    ; SAL EDX,16 
+    ; OR EAX,EDX 
+    ; MOV ECX,EAX 
+    ; MOVSX EAX,BL 
+    ; IMUL ECX 
+    ; ADD EAX,EDI 
+    ; SUB EAX,56d 
+    ; CDQ 
+    ; IDIV ESI 
     
-    MOV AL,CL 
-    IMUL BL 
-    CWDE 
-    SAL EAX,3 
-    MOV EDI,EAX 
-    MOVSX AX,BH 
-    MOV SI,AX 
-    SAL SI,6 
-    ADD SI,AX 
-    SAL AX,3 
-    ADD SI,AX 
-    MOVSX ESI,SI 
-    SUB ESI,EDI 
-    MOVSX EAX,BL 
-    ADD EAX,ESI 
-    JZ znamenatelzero 
-    MOV ESI,EAX 
-    MOV AL,CL 
-    IMUL CL 
-    CWDE 
-    MOV EDI,EAX 
-    MOV AL,BH 
-    IMUL BH 
-    MOV CX,AX 
-    MOVSX AX,BH 
-    IMUL CX 
-    SAL EDX,16 
-    OR EAX,EDX 
-    MOV ECX,EAX 
-    MOVSX EAX,BL 
-    IMUL ECX 
-    ADD EAX,EDI 
-    SUB EAX,56d 
-    CDQ 
-    IDIV ESI 
-    
-    mov [bp+4], eax 
-    pop bp 
+    ; mov [bp+4], eax 
+    ; pop bp 
     ret 8 
-    vizov endp 
+    calc endp 
   
 end start
