cc41132d887a79d03cf4582684bf21250f8f7ce3
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-11-30 20:46:12 +0300

    Create test.asm

---
+++ Course-wrk/MyDis/test.asm
@@ -0,0 +1,46 @@
+.MODEL      SMALL
+
+B_SEG       SEGMENT
+
+ORG         100h
+ASSUME      DS:B_SEG, CS:B_SEG, SS:B_SEG
+
+START:
+    ; Segment override tests
+    MOV     AX, ES:[2h]
+    MOV     AX, CS:[2h]
+    MOV     AX, DS:[BP + 2h]
+    MOV     AX, DS:[2h]
+    MOV     AX, SS:[2h]
+        ; MOV tests
+    MOV     DX, 1234h
+    MOV     BYTE PTR [BX + DI + 21h], 78h
+    MOV     DX, WORD PTR ES:[645Eh]
+    MOV     BX, DX
+    MOV     ES, WORD PTR CS:[0FFh]
+    MOV     SS, CX
+    MOV     AX, WORD PTR DS:[3h]
+    MOV     DS:[12h], AL
+    ; Conditional jump tests
+    JO      CONDITIONAL_JMP_LABEL
+    JNO     CONDITIONAL_JMP_LABEL
+    JB      CONDITIONAL_JMP_LABEL
+    JAE     CONDITIONAL_JMP_LABEL
+    JE      CONDITIONAL_JMP_LABEL
+    JNE     CONDITIONAL_JMP_LABEL
+    JBE     CONDITIONAL_JMP_LABEL
+    JA      CONDITIONAL_JMP_LABEL
+    JS      CONDITIONAL_JMP_LABEL
+    JNS     CONDITIONAL_JMP_LABEL
+    JP      CONDITIONAL_JMP_LABEL
+    JNP     CONDITIONAL_JMP_LABEL
+    JL      CONDITIONAL_JMP_LABEL
+    JGE     CONDITIONAL_JMP_LABEL
+    JLE     CONDITIONAL_JMP_LABEL
+    JG      CONDITIONAL_JMP_LABEL
+    CONDITIONAL_JMP_LABEL:
+    LOCK    MOV BX, AX
+ 
+B_SEG       ENDS
+
+END         START
