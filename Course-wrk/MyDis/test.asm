e257b8a2d910a7527e7efdb4880c1106cc62e9be
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-12-06 15:20:01 +0300

    changed to normal template

--- Course-wrk/MyDis/test.asm
+++ Course-wrk/MyDis/test.asm
@@ -1,13 +1,35 @@
 .MODEL      SMALL
-
-B_SEG       SEGMENT
-
-ORG         100h
-ASSUME      DS:B_SEG, CS:B_SEG, SS:B_SEG
+.486
+; B_SEG       SEGMENT
+.code
+    ORG         100h
+; ASSUME      DS:B_SEG, CS:B_SEG, SS:B_SEG
 
 START:
+    ; ; Conditional jump tests
+    LOCK    MOV BX, AX
+    db 2Eh, 70h, 0eeh
+    db 3Eh, 70h, 0eeh
+    AAD
+    ; JO      CONDITIONAL_JMP_LABEL
+    ; JNO     CONDITIONAL_JMP_LABEL
+    ; JB      CONDITIONAL_JMP_LABEL
+    ; JAE     CONDITIONAL_JMP_LABEL
+    ; JE      CONDITIONAL_JMP_LABEL
+    ; JNE     CONDITIONAL_JMP_LABEL
+    ; JBE     CONDITIONAL_JMP_LABEL
+    ; JA      CONDITIONAL_JMP_LABEL
+    ; JS      CONDITIONAL_JMP_LABEL
+    ; JNS     CONDITIONAL_JMP_LABEL
+    ; JP      CONDITIONAL_JMP_LABEL
+    ; JNP     CONDITIONAL_JMP_LABEL
+    ; JL      CONDITIONAL_JMP_LABEL
+    ; JGE     CONDITIONAL_JMP_LABEL
+    ; JLE     CONDITIONAL_JMP_LABEL
+    ; JG      CONDITIONAL_JMP_LABEL
     ; Segment override tests
-    MOV     AX, ES:[2h]
+    ; mov     ax, fs:[2h]
+    ; mov     bx, gs:[-1]
     MOV     AX, CS:[2h]
     MOV     AX, DS:[BP + 2h]
     MOV     AX, DS:[2h]
@@ -21,26 +43,10 @@ START:
     MOV     SS, CX
     MOV     AX, WORD PTR DS:[3h]
     MOV     DS:[12h], AL
-    ; Conditional jump tests
-    JO      CONDITIONAL_JMP_LABEL
-    JNO     CONDITIONAL_JMP_LABEL
-    JB      CONDITIONAL_JMP_LABEL
-    JAE     CONDITIONAL_JMP_LABEL
-    JE      CONDITIONAL_JMP_LABEL
-    JNE     CONDITIONAL_JMP_LABEL
-    JBE     CONDITIONAL_JMP_LABEL
-    JA      CONDITIONAL_JMP_LABEL
-    JS      CONDITIONAL_JMP_LABEL
-    JNS     CONDITIONAL_JMP_LABEL
-    JP      CONDITIONAL_JMP_LABEL
-    JNP     CONDITIONAL_JMP_LABEL
-    JL      CONDITIONAL_JMP_LABEL
-    JGE     CONDITIONAL_JMP_LABEL
-    JLE     CONDITIONAL_JMP_LABEL
-    JG      CONDITIONAL_JMP_LABEL
+
     CONDITIONAL_JMP_LABEL:
     LOCK    MOV BX, AX
  
-B_SEG       ENDS
+; B_SEG       ENDS
 
 END         START
