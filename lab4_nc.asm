962254d2c2d233da68145352538c923986106f22
Ice <iceisnicehq@gmail.com>
2024-12-24 14:32:52 +0300

    no spaces

--- lab4_nc.asm
+++ lab4_nc.asm
@@ -10,30 +10,30 @@
 Start:
     mov     ax, @data
     mov     ds, ax
-    fld     C                  
-    fld     A                  
-    fld1                       
-    fadd    st(0), st(0)       
-    fld     st(0)              
-    fscale                     
-    fxch    st(1)              
-    fsubp   st(1), st(0)       
-    fld     st(2)              
-    fscale                     
-    fadd    st(0), st(3)       
-    fld     st(1)              
-    fmul    st(0), st(3)       
-    fadd    st(0), st(3)       
-    fmul    st(0), st(3)       
-    faddp                      
-    jz      SHORT  exit    
-    fld     B                  
-    fadd    st(0), st(0)       
-    fmul    st(0), st(2)       
-    fmul    st(0), st(4)       
-    fadd    st(0), st(3)       
-    fadd    st(0), st(2)       
-    fdiv    st(0), st(1)       
+    fld     C
+    fld     A
+    fld1
+    fadd    st(0), st(0)
+    fld     st(0)
+    fscale
+    fxch    st(1)
+    fsubp   st(1), st(0)
+    fld     st(2)
+    fscale
+    fadd    st(0), st(3)
+    fld     st(1)
+    fmul    st(0), st(3)
+    fadd    st(0), st(3)
+    fmul    st(0), st(3)
+    faddp
+    jz      SHORT  exit
+    fld     B
+    fadd    st(0), st(0)
+    fmul    st(0), st(2)
+    fmul    st(0), st(4)
+    fadd    st(0), st(3)
+    fadd    st(0), st(2)
+    fdiv    st(0), st(1)
     fistp   D
 Exit:
     mov     ah, 4ch
