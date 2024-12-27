0894c1d86cb1fbf874e3467fe056b0b3cba8b344
Ice <iceisnicehq@gmail.com>
2024-12-27 22:24:05 +0300

    coms

--- lab_II.asm
+++ lab_II.asm
@@ -5,8 +5,8 @@
 maxSize                 EQU     256
 cr                      EQU     0Dh
 lf                      EQU     0Ah
-NUMBER_WORD_TO_STORE    EQU      3  ;actual 4
-REVERSED_WORD_NUMBER    EQU      2  ;actual 3
+NUMBER_WORD_TO_STORE    EQU      3  ; actual 4
+REVERSED_WORD_NUMBER    EQU      2  ; actual 3
 
 .data
     file                db      'RES.TXT', 0
@@ -20,87 +20,86 @@ start:
     mov     ax, @data
     mov     ds, ax
     mov     es, ax 
-    mov     ax, 0003h
+    mov     ax, 0003h ; 80x25 ochistka
     int     10h
     mov     ah, 09h
     mov     dx, offset input_message
     int     21h  
-    mov     si, OFFSET buffer
-    mov     bp, si
-    mov     di, si
+    mov     si, OFFSET buffer ; si for reversing (if word exist)
+    mov     bp, si            ; for moving into registers
+    mov     di, si            ; for storing 
     mov     al, " "
-    mov     cx, maxSize
-    mov     dl, NUMBER_WORD_TO_STORE
+    mov     cx, maxSize+1
     mov     bl, REVERSED_WORD_NUMBER
 store_byte:
-    cmp     al, " "
-    je      bios_input
+    dec     cx              ; decrease number of chars
+    cmp     al, " "         ; if word starts with space
+    je      bios_input      ;   then dont save it
     stosb
 bios_input:
-    mov     dh, al
+    mov     dh, al          ; save last char
 key_input:
-    xor     ah, ah
-    int     16h 
-    cmp     al, cr     
+    xor     ah, ah          ; func bios 
+    int     16h             ; interrupt bios
+    cmp     al, cr          ; if enter (0dh)
     je      end_of_line
-    cmp     al, "~"
-    ja      key_input   
-    cmp     al, " "
-    jb      key_input
-    jne     not_space
-    cmp     dh, al
-    je      bios_input
-    inc     bh
+    cmp     al, "~"         ; 07Eh last ascii char (20h-7Eh)
+    ja      key_input       
+    cmp     al, " "         ; check if space
+    jb      key_input       ; if ctrl+key
+    jne     not_space       ; not space
+    cmp     dh, al          ; check if last char is space
+    je      bios_input      ; if last is space jump to key input 
+    inc     bh              ; bh = number of spaces (number of words)
 not_space:
-    mov     ah, 0Eh
-    int     10h
-    cmp     bh, NUMBER_WORD_TO_STORE
-    jb      to_loop
-    je      store_byte
-    stosb
-    xor     bh, bh
-    dec     bl
+    mov     ah, 0Eh         ; bios func for printing
+    int     10h             ; video display interrupt 
+    cmp     bh, NUMBER_WORD_TO_STORE ; if number of words is 4
+    jb      to_loop         ; if words less than 4, then jump to iteration
+    je      store_byte      ; if the word is 4th, then store in memory
+    stosb                   ; store space in memory (end of word)
+    xor     bh, bh          ; bh = 0 (number of words)
+    dec     bl              ; word to reverse
     jnz     to_loop
-    mov     si, di
+    mov     si, di          ; save address of word to reverse
 to_loop:
     loop    bios_input  
     mov     ah, 09h
     mov     dx, offset size_limit
     int     21h
 end_of_line:
-    cmp     bp, di
+    cmp     bp, di          ; if no words were saved (number of words < 4)
     jne     no_short_string
     mov     ah, 09h
     mov     dx, offset too_short
     int     21h
     jmp     exit
 no_short_string:
-    mov     al, " "
-    cmp     byte ptr [di-1], al
+    mov     al, " "        
+    cmp     byte ptr [di-1], al ; if last char is space
     je      last_space
-    stosb
+    stosb                       ; if not space then put space in memory
 last_space:
-    cmp     [si], al
-    jbe     output
-    cmp     si, bp
+    cmp     [si], al            ; check if space in word to reverse (it is only " ")
+    jbe     output              ; if space or 00h then jump to output
+    cmp     si, bp              ; if number of words is less then < poryadok (reversed word) then jmp to output
     je      output
-    mov     di, si
-    not     cx
-    repne   scasb
+    mov     di, si              ; di = address of word to reverse
+    mov     cx, -1              ; just to make cx big
+    repne   scasb               
     dec     di
 reversing:
     dec     di
-    cmp     si, di
+    cmp     si, di              ; if nechetnoe chislo bukv
     jz      output
-    mov     al, [si]
+    lodsb
     mov     ah, [di]
-    mov     [si], ah
+    mov     [si-1], ah
     mov     [di], al
-    inc     si
-    cmp     si, di
+    cmp     si, di              ; if chetnoe chislo bukv
     jne     reversing
 output:
-    mov     ah, 09h
+    mov     ah, 09h 
     mov     dx, offset output_message
     int     21h
     mov     ah, 3Ch
@@ -112,16 +111,16 @@ output:
     mov     dx, bp
     mov     di, dx
     mov     cx, 0ffffh
-    repne   scasb
+    repne   scasb ; 0ffah -> 0005h -> 
     not     cx
     dec     cx
     dec     cx
-    int     21h
+    int     21h   ; zapis v file
     mov     ah, 3Eh
     int     21h
     mov     ah, 40h
-    mov     bx, 1
-    int     21h
+    mov     bx, 1 ; standard video display output
+    int     21h   ; vivod na ekran
 exit:
     xor     ah, ah
     int     16h 
