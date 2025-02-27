6e3c1945174175206deb80415b4a7f12977ed335
iceisnicehq <iceisnicehq@gmail.com>
2025-02-27 23:38:59 +0300

    done_check

--- l2/sr.asm
+++ l2/sr.asm
@@ -4,120 +4,117 @@
 
 .data
     answer        db      "answer.txt", 0
-    info_out      db      cr, lf, "___OUTPUT___$"
-    warning       db      cr, lf, "Not enough words to convert string. Exiting.$"
-    info_in       db      "Enter text: ", "$"
-    buffer        db      255 DUP(?)
+    info_out      db      13, 10, "Formatted text: $"
+    warning       db      13, 10, "Minimal len = 6 words.$"
+    info_in       db      "Raw text: ", "$"
+    exit_msg 	  db      13, 10, 13, 10, "Press any key...$"
+    buffer        db      255 DUP(" ")
 
 .code
 start:
     mov     ax, @DATA
     mov     ds, ax
-    mov     es, ax 
-    mov     ax, 0003h ; 80x25 ochistka
-    int     10h
-    mov     ah, 09h
+    mov     es, ax
+    mov     ah, 9
     mov     dx, offset info_in
-    int     21h  
-    mov     si, OFFSET buffer ; si for reversing (if word exist)
-    mov     bp, si            ; for moving into registers
-    mov     di, si            ; for storing 
-    mov     al, " "
-    mov     cx, maxSize+1
-    mov     bl, REVERSED_WORD_NUMBER
-store_byte:
-    dec     cx              ; decrease number of chars
-    cmp     al, " "         ; if word starts with space
-    je      bios_input      ;   then dont save it
+    int     21h
+    mov     si, offset buffer   ; si для проверки далее (cmp si, di), по итогу si = адрес начала слова для инвертирования
+    mov     di, si              ; для записи в es:di
+    mov     al, 32              ; 32 = аски пробел, сделали для ниже cmp al, 32 
+    mov     cx, 255		; число циклов для записи
+    xor     bl, 5		; для уверенности (как ве любит)
+    mov     dl, 1               ; каждое 6 слово
+    jmp     bios_input          ; прыжок на считку
+store_byte:  			; эта метка работает далее, для записи, поэтому далььше dec cx (записали, уменьшили число циклов)
+    dec     dl
+    jnz     not_first_word
+    mov     si, di
+not_first_word:
+    dec     cx                  ; уменьшили число циклов
+    cmp     al, 32              ; тут, чтобы исключить запись пробелов перед словом и после слова (двойные пробелы слово1__слово2)
+    je      bios_input          ; не сохраняем пробел ЭТИМ стосб
     stosb
 bios_input:
-    mov     dh, al          ; save last char
+    mov     bh, al              ; в dh сохраняется последний записанный символ (нужно для контроля пробелов)
 key_input:
-    xor     ah, ah          ; func bios 
-    int     16h             ; interrupt bios
-    cmp     al, cr          ; if enter (0dh)
-    je      end_of_line
-    cmp     al, "~"         ; 07Eh last ascii char (20h-7Eh)
-    ja      key_input       
-    cmp     al, " "         ; check if space
-    jb      key_input       ; if ctrl+key
-    jne     not_space       ; not space
-    cmp     dh, al          ; check if last char is space
-    je      bios_input      ; if last is space jump to key input 
-    inc     bh              ; bh = number of spaces (number of words)
+    xor     ah, ah              ; функция биоса для считки клавиатуры (ah=00)
+    int     16h                 ; прерывание клавиатуры (можно загуглить int 16h)
+    cmp     al, 13              ; 13=enter
+    je      end_of_line		; нажали enter - вышли 
+    cmp     al, 128  	        ; 07Eh last ascii char
+    jae     key_input       
+    cmp     al, 32    	        ; check if space
+    jb      key_input           ; if ctrl+key
+    jne     not_space           ; not space
+    cmp     bh, al              ; check if last char is space
+    je      bios_input          ; if last is space then ignore it, and start reading chars again
+    dec     bl                  ; bl = число слов
 not_space:
-    mov     ah, 0Eh         ; bios func for printing
-    int     10h             ; video display interrupt 
-    cmp     bh, NUMBER_WORD_TO_STORE ; if number of words is 4
-    jb      to_loop         ; if words less than 4, then jump to iteration
-    je      store_byte      ; if the word is 4th, then store in memory
-    stosb                   ; store space in memory (info_out of word)
-    xor     bh, bh          ; bh = 0 (number of words)
-    dec     bl              ; word to reverse
-    jnz     to_loop
-    mov     si, di          ; save address of word to reverse
+    mov     ah, 14              ; bios func for printing
+    int     10h                 ; video display interrupt 
+    or      bl, bl              ; если бл=0, то уже записали пять слов, следовательно след слово - 6-ое
+    jz      store_byte          ; тогда записываем  
+    jns     to_loop             ; за бл=0, только бл -1, там уже новые 6 слов, и надо восстановить счетчик бл
+    mov     bl, 5               
 to_loop:
     loop    bios_input  
-    mov     ah, 09h
-    mov     dx, offset size_limit
-    int     21h
 end_of_line:
-    cmp     bp, di          ; if no words were saved (number of words < 4)
+    cmp     di, offset buffer   ; if no words were saved (number of words < 4)
     jne     no_short_string
-    mov     ah, 09h
+    mov     ah, 9
     mov     dx, offset warning
     int     21h
     jmp     exit
 no_short_string:
-    mov     al, " "        
-    cmp     byte ptr [di-1], al ; if last char is space
-    je      last_space
-    stosb                       ; if not space then put space in memory
-last_space:
-    cmp     [si], al            ; check if space in word to reverse (it is only " ")
-    jbe     output              ; if space or 00h then jump to output
-    cmp     si, bp              ; if number of words is less then < poryadok (reversed word) then jmp to output
-    je      output
-    mov     di, si              ; di = address of word to reverse
-    mov     cx, -1              ; just to make cx big
-    repne   scasb               
-    dec     di
+    mov     dx, di              ; сохраняем адрес  конца, чтоб потом найти длину всей строки
+    mov     al, 32
+    cmp     [si], al		; случай, когда мы дошли до слова для инвертирования, но после него просто нажали пробел и его адрес это пустота (то есть слова нет, но адрес начала у нас есть)
+    jbe     output              ; if space then jump to output
+;    cmp     si, offset buffer              ; случай общий, но не для тебя, тут кейс такой, что если бы у тебя было например второе слово, то после записи одного слова и нажатия энтера, реверсать было бы нечего, поэтому сам реверс надо скипнуть
+ ;   je      output
+    mov     di, si              ; di = address of the word to reverse
+    mov     cx, 255
+find_reverse_len:
+    scasb
+    loopne  find_reverse_len
+    dec     di  		; тут дек, так как мы за пробелом в памяти
 reversing:
-    dec     di
-    cmp     si, di              ; if nechetnoe chislo bukv
+    dec     di			; а тут с пробела переходим на уже буквы слова
+    cmp     si, di              ; проверка для нечетной длины
     jz      output
     lodsb
     mov     ah, [di]
     mov     [si-1], ah
     mov     [di], al
-    cmp     si, di              ; if chetnoe chislo bukv
+    cmp     si, di              ; для четной длины
     jne     reversing
 output:
-    mov     ah, 09h 
+    mov     cx, dx
+    sub     cx, offset buffer 
+    mov     ah, 9 
     mov     dx, offset info_out
     int     21h
     mov     ah, 3Ch
-    mov     dx, offset answer
+    mov     di, cx
     xor     cx, cx
+    mov     dx, offset answer
     int     21h
+    mov     cx, di
     mov     bx, ax
-    mov     ax, 4000h
-    mov     dx, bp
-    mov     di, dx
-    mov     cx, 0ffffh
-    repne   scasb ; 0ffah -> 0005h -> 
-    not     cx
-    dec     cx
-    dec     cx
-    int     21h   ; zapis v answer
-    mov     ah, 3Eh
+    mov     dx, offset buffer
+    mov     ax, 40h
+    int     21h   		; zapis v answer
+    mov     ah, 3Eh		; закрываем файл
     int     21h
     mov     ah, 40h
-    mov     bx, 1 ; standard video display output
-    int     21h   ; vivod na ekran
+    mov     bx, 1	 	; standard video display output
+    int     21h   		; vivod na ekran
 exit:
-    xor     ah, ah
-    int     16h 
-    mov     ax, 4C00h      
+    mov     ah, 9
+    mov     dx, offset exit_msg
     int     21h
-    info_out     start
+    xor     ah, ah		; тут используем функцию для ввода, но ничего не сохраняем, просто чтоб пользователь нажал и прога завершилась, а не сразу после нажатия энтера выше
+    int     16h
+    mov     ah, 4Ch 
+    int     21h
+    end     start
