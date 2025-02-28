9844b9ca4db6e6b730c214f0b2448473f34d2205
iceisnicehq <iceisnicehq@gmail.com>
2025-02-28 23:42:20 +0300

    for shufling

--- icc/com.asm
+++ icc/com.asm
@@ -3,93 +3,93 @@
 .CODE
     ORG     100H
 START:
-	CWDE
-	NEG    EAX
-	NEG    word ptr CS:[EAX+EBX*4+0ABCDH]
-	NEG    dword ptr DS:[BX+SI]
-	NEG    word ptr FS:[EBP+EDI*8]
-	NEG    byte ptr CS:[EDI]
-	NEG    dword ptr ES:[DI+0ABCDH]
-	NEG    AL
-	NEG    word ptr ES:[SI]
-	NEG    word ptr FS:[11111H]
-	NEG    word ptr DS:[BP]
-	NEG    word ptr SS:[BP+DI]
-	NEG    dword ptr GS:[ESP+EBP*4+1H]
-	NEG    dword ptr FS:[ESP+EBP]
-	NEG    byte ptr SS:[BP+DI]
-	NEG    byte ptr SS:[SI]
-	NEG    byte ptr FS:[EDX+EBX+1214H]
-	NEG    dword ptr DS:[EAX+EBP*8]
-	NEG    word ptr SS:[EDI+5678H]
-	NEG    word ptr DS:[EBX+ECX*4]
-	NEG    dword ptr FS:[EDI+ESI]
-	NEG    dword ptr GS:[BP+100H]
-	NEG    dword ptr CS:[ESI+0ABCDH]
-	NEG    EBX
-	NEG    DH
-	NEG    dword ptr ES:[ESP]
-	NEG    byte ptr DS:[BX+1234H]
-	NEG    AX
-	NEG    word ptr CS:[SI+0ABCDH]
-	NEG    dword ptr ES:[EDI+ESI]
-	NEG    word ptr CS:[EBP*4]
-	NEG    byte ptr ES:[DI]
-	NEG    dword ptr SS:[BP+DI+100H]
-	NEG    word ptr SS:[BX+SI+1234H]
-	NEG    dword ptr DS:[EBX+EDX]
-	NEG    ESP
-	NEG    byte ptr FS:[EDI]
-	NEG    word ptr SS:[BP]
-	NEG    byte ptr DS:[123H]
-	NEG    byte ptr GS:[EBP+ESI]
-	NEG    byte ptr CS:[BP+SI]
-	NEG    AH
-	NEG    byte ptr GS:[EBP+EDI*8]
-	NEG    word ptr FS:[EDI+ESI+5678H]
-	NEG    word ptr ES:[EAX+EBX*4]
-	NEG    BX
-	NEG    BP
-	NEG    word ptr DS:[EBX+11H]
-	NEG    byte ptr FS:[ESP+1010h]
-	NEG    word ptr CS:[EAX+EBX*4]
-	NEG    dword ptr GS:[EBP]
-	NEG    byte ptr SS:[BP+SI]
-	NEG    word ptr GS:[EBP+EDI*8+1000H]
-	NEG    byte ptr FS:[EBP+1000H]
+    CDQ
+    CALL   REL_CALL
 REL_CALL:
-	CALL   word ptr ES:[DI+10H]
-	CALL   dword ptr ES:[EBX+EAX]
-	CALL   START
-	CALL   REL_CALL
-	CALL   dword ptr GS:[EBX+ESI+10101H]
-	CALL   AX
-	CALL   REL_CALL
-	CALL   $+212H
-	CALL   $-1H
-	CALL   word ptr DS:[BX+100H]
-	CALL   dword ptr SS:[EBX+ESI+111H]
-	CALL   dword ptr GS:[EAX+EAX*8+1H]
-	CALL   $+1234H
-	CALL   word ptr SS:[BX+1010H]
-	CALL   $+0ABCH
-	CALL   $-1001H
-        CALL   word ptr FS:[BX+DI+1H]
-	CALL   EBX
-	CALL   word ptr FS:[EBP]
-	CALL   CX
-	CALL   dword ptr DS:[EDX]
-	CALL   dword ptr GS:[EBX+EDI*8+1010H]
-	CALL   dword ptr CS:[EDI*8+1111H]
-	CALL   word ptr FS:[BP+SI]
-	CALL   word ptr GS:[EBX*2]
-	CALL   word ptr SS:[BX+SI]
-	CALL   $-212H
-	DB     9Ah, 56h, 78h, 12h, 34h
-	CALL   EDX
-	DB     66h, 9Ah, 0AAh, 0BBh, 0CCh, 0DDh, 0EEh, 0FFh
-	CALL   dword ptr GS:[ECX]
-	DB     66h, 0E8h, 10h, 10h, 10h, 10h
-	CALL   $+99H
-	DB     66h, 0E8h, 0FFh, 0FFh, 0FFh, 0FFh
+    CALL   dword ptr CS:[DI+10H]
+    CALL   AX
+    CALL   word ptr CS:[ESI+EAX]
+    CALL   dword ptr GS:[ESI+ESI+10101H]
+    CALL   START
+    CALL   REL_CALL
+    CALL   $+212H
+    CALL   $-1H
+    CALL   word ptr DS:[BX+100H]
+    CALL   dword ptr SS:[ESI+ESI+111H]
+    CALL   dword ptr GS:[EAX+EAX*8+1H]
+    CALL   $+1234H
+    CALL   word ptr SS:[BX+1010H]
+    CALL   $+0ABCH
+    CALL   $-1001H
+    CALL   word ptr FS:[BX+DI+1H]
+    CALL   ESI
+    CALL   word ptr FS:[EBP]
+    CALL   CX
+    CALL   dword ptr DS:[EDX]
+    CALL   dword ptr GS:[ESI+EDI*8+1010H]
+    CALL   dword ptr CS:[EDI*8+1111H]
+    CALL   word ptr FS:[BP+SI]
+    CALL   word ptr GS:[ESI*2]
+    CALL   word ptr SS:[BX+SI]
+    CALL   $-212H
+    DB     9Ah, 56h, 78h, 12h, 34h
+    CALL   EDX
+    DB     66h, 9Ah, 0A0h, 0B0h, 0C0h, 0D0h, 0E0h, 0F0h
+    CALL   dword ptr GS:[ECX]
+    DB     66h, 0E8h, 10h, 1h, 1h, 10h
+    DB     66h, 0E8h, 0Ah, 011h, 011h, 011h
+    IDIV   CL
+    IDIV   word ptr CS:[EAX+ESI*4+0ABCDH]
+    IDIV   EAX
+    IDIV   dword ptr DS:[BX+SI]
+    IDIV   word ptr FS:[EBP+EDI*8]
+    IDIV   byte ptr CS:[EDI]
+    IDIV   dword ptr CS:[DI+0ABCDH]
+    IDIV   AL
+    IDIV   word ptr ES:[SI]
+    IDIV   word ptr FS:[11111H]
+    IDIV   word ptr DS:[BP]
+    IDIV   word ptr SS:[BP+DI]
+    IDIV   dword ptr GS:[ESP+EBP*4+1H]
+    IDIV   dword ptr FS:[ESP+EBP]
+    IDIV   byte ptr SS:[BP+DI]
+    IDIV   byte ptr SS:[SI]
+    IDIV   byte ptr FS:[EDX+EBX+1214H]
+    IDIV   dword ptr DS:[EAX+EBP*8]
+    IDIV   word ptr SS:[EDI+5678H]
+    IDIV   word ptr DS:[ESI+ECX*4]
+    IDIV   dword ptr FS:[EDI+ESI]
+    IDIV   dword ptr GS:[BP+100H]
+    IDIV   dword ptr CS:[ESI+0ABCDH]
+    IDIV   ESI
+    IDIV   DH
+    IDIV   dword ptr ES:[ESP]
+    IDIV   byte ptr DS:[BX+1234H]
+    IDIV   AX
+    IDIV   word ptr CS:[SI+0ABCDH]
+    IDIV   dword ptr CS:[EDI+ESI]
+    IDIV   word ptr CS:[EBP*4]
+    IDIV   byte ptr CS:[DI]
+    IDIV   dword ptr SS:[BP+DI+100H]
+    IDIV   word ptr SS:[BX+SI+1234H]
+    IDIV   dword ptr DS:[ESI+EDX]
+    IDIV   ESP
+    IDIV   byte ptr FS:[EDI]
+    IDIV   word ptr SS:[BP]
+    IDIV   byte ptr DS:[123H]
+    IDIV   byte ptr GS:[EBP+ESI]
+    IDIV   byte ptr CS:[BP+SI]
+    IDIV   AH
+    IDIV   byte ptr GS:[EBP+EDI*8]
+    IDIV   word ptr FS:[EDI+ESI+5678H]
+    IDIV   word ptr CS:[EAX+ESI*4]
+    IDIV   BX
+    IDIV   BP
+    IDIV   word ptr DS:[ESI+11H]
+    IDIV   byte ptr FS:[ESP+1010h]
+    IDIV   word ptr CS:[EAX+EBX*4]
+    IDIV   dword ptr GS:[EBP]
+    IDIV   byte ptr SS:[BP+SI]
+    IDIV   word ptr GS:[EBP+EDI*8+1000H]
+    IDIV   byte ptr FS:[EBP+1000H]
 END START
