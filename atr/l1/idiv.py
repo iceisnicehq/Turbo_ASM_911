ae14d8d2400ab0c250076f955201946a4249072c
iceisnicehq <iceisnicehq@gmail.com>
2025-04-12 18:21:15 +0300

    cleanup

--- atr/l1/idiv.py
+++
@@ -1,8 +0,0 @@
-count = 0
-for c in range(-128, 128):
-    for b in range(-128, 128):
-        for a in range(-128, 128):
-            delen = (2*c*c) + (13 * b)
-            if delen > 32767 or delen < -32768:
-                count += 1
-print(count)
--- atr/l1_art/lab1.py
+++
@@ -1,4 +0,0 @@
-while True:
-    A, B, C = int(input('A: ')), int(input('B: ')), int(input('C: '))
-
-    print((44*A*B*B + 12*C - A*A) / (2*C*C + 13*B))
